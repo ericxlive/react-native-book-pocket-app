7f048ad2099be09521f6eb44c104f251cf892492
Eric de Souza <ericdsky@gmail.com>
2018-06-07 21:14:14 -0300

    Adding books

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import Bookcase from './src/components/Bookcase';
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <Bookcase />
       </View>
     );
   }
--- src/components/Bookcase.js
+++ src/components/Bookcase.js
@@ -1,30 +1,73 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import {
+  StatusBar,
+  StyleSheet,
+  FlatList,
+  Text,
+  View
+} from 'react-native';
 
-class Bookcase extends Component {
-    render(){
-        return(
-            <View>
-                <Text>
-                    Bookcase.js
-                </Text>
-            </View>
-        );
+import BookcaseItem from './BookcaseItem';
+
+export default class Boookcase extends Component {
+    
+  constructor(props) {
+    super(props);
+    this.state = {
+      books: [
+        {
+          id: 1,
+          title: 'Harry Potter and the Goblet of Fire',
+          author: 'J. K. Rowling',
+          thumbnail: 'https://covers.openlibrary.org/w/id/7984916-M.jpg'
+        },
+        {
+          id: 2,
+          title: 'The Hobbit',
+          author: 'J. R. R. Tolkien',
+          thumbnail: 'https://covers.openlibrary.org/w/id/6979861-M.jpg'
+        },
+        {
+          id: 3,
+          title: '1984',
+          author: 'George Orwell',
+          thumbnail: 'https://covers.openlibrary.org/w/id/7222246-M.jpg'
+        }
+      ]
     }
+  }
+
+  _renderItem = ({item}) => (
+    <BookcaseItem
+      id={item.id}
+      title={item.title}
+      author={item.author}
+      thumbnail={item.thumbnail}
+    />
+  );
+
+  _keyExtractor = (item, index) => item.id;
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <StatusBar
+          barStyle="light-content"
+        />
+        <FlatList
+          data={this.state.books}
+          keyExtractor={this._keyExtractor}
+          renderItem={this._renderItem}
+        />
+      </View>
+    );
+  }
 }
 
-const styles = {
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#F5FCCF',
-    },
-    title: {
-        fontSize: 20,
-        textAlign: 'center',
-        margin: 10,
-    }
-}
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#F5FCFF',
+  }
+});
 
-export default Bookcase;
